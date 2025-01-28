68c3e803894f34b4e312d2053b5f0146725be552
Arjun Thakur <thakurarjun247@gmail.com>
2025-01-28 19:00:54 +0530

    more fetch endpoints.

--- 14.ErrorException/fetch.js
+++ 14.ErrorException/fetch.js
@@ -1,9 +1,19 @@
+//prerequisite:
+// 1. run this on terminal: npm install node-fetch
+// 2. create a file package.json with the content: 
+//{  "type": "module"}
+
+//why prerequistes?
+//1. which is necessary because Node.js doesn’t have a built-in fetch API (as of now). Unlike browsers, which natively support fetch, you need to rely on third-party libraries like node-fetch in Node.js.
+//2. The "type": "module" setting tells Node.js to treat all .js files in your project as ES modules (ECMAScript modules)
 // Import node-fetch using ES module syntax
 import fetch from "node-fetch";
 
 // API for get requests
 let fetchRes = fetch(
     "https://jsonplaceholder.typicode.com/todos/1");
+
+    //more here: https://jsonplaceholder.typicode.com/posts/1
         
     // FetchRes is the promise to resolve
     // it by using.then() method
