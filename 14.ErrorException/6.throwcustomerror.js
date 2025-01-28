e4b6c525304dac85c6cfbaebca291a3113012d7b
Arjun Thakur <thakurarjun247@gmail.com>
2025-01-28 18:57:37 +0530

    custom exception, fetch api

---
+++ 14.ErrorException/6.throwcustomerror.js
@@ -0,0 +1,21 @@
+// Define a custom error class
+class DuplicateEmailError extends Error {
+    constructor(message) {
+        super(message); // Call the parent class constructor
+        this.name = "DuplicateEmailError"; // Set the error name
+    }
+}
+
+const email="yuvan@gmail.com";
+try{
+    //fetch list of existing emails from db
+    if(email=="yuvan@gmail.com")
+        {
+            throw new DuplicateEmailError("choose another email");
+        }
+}
+catch(err){
+    console.log(err.name);
+    console.log(err.message);
+}
+
---
+++ 14.ErrorException/fetch.js
@@ -0,0 +1,14 @@
+// Import node-fetch using ES module syntax
+import fetch from "node-fetch";
+
+// API for get requests
+let fetchRes = fetch(
+    "https://jsonplaceholder.typicode.com/todos/1");
+        
+    // FetchRes is the promise to resolve
+    // it by using.then() method
+    fetchRes.then(res =>
+        res.json()).then(d => {
+            console.log(d)
+        })
+    
