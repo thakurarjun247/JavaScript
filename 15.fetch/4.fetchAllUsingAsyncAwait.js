211de33f8c65f75af493e9ac484e31fceebbe0b0
Arjun Thakur <thakurarjun247@gmail.com>
2025-02-06 15:16:04 +0530

    quote api

--- 15.fetch/4.fetchAllUsingAsyncAwait.js
+++ 15.fetch/4.fetchAllUsingAsyncAwait.js
@@ -5,7 +5,8 @@ async function fetchUsers() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
-    users.forEach(console.log);
+    //users.forEach(console.log);
+    //can be written as 
     users.forEach(user => console.log(user));
   } catch (error) {
     console.error("Error fetching users:", error);
