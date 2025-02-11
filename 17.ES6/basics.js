70052a909d1c7494220ad91147755cc33ff3df24
Arjun Thakur <thakurarjun247@gmail.com>
2025-02-11 16:19:46 +0530

    weather project

--- 17.ES6/basics.js
+++ 17.ES6/basics.js
@@ -10,16 +10,26 @@ console.log(second);
 let book = { title: "Atomic Habits", author: "James Clear", pages: 320 };
 let {title, author}=book;
 console.log(author);
+
+//spread operator
 let numbers1 = [1, 2, 3];
 let numbers2 = [4, 5, 6];
 
 let allNumbers = [...numbers1, ...numbers2];
 console.log(allNumbers);
+
 let product = { id: 101, name: "Laptop", price: 50000 };
-const x=Object.entries(product);
+const x=Object.entries(product); //object -> array
+
 
 x.forEach(([key, value]) => {
     console.log(`${key}: ${value}`);
   });
 
 
+  let words = ["hello world", "good,morning"];
+  //[“hello”, “world”]
+  let splitWords = words.flatMap(sentence => sentence.split(","));
+  
+  console.log(splitWords); // Output: ["hello", "world", "good", "morning"]
+  
---
+++ 19.project/weather.js
@@ -0,0 +1,28 @@
+
+
+import fetch from "node-fetch";
+
+async function getWeatherData() {
+  const city = "London"; // Hardcoded city, get it from user
+
+  try {
+    const response = await fetch(`https://wttr.in/${city}?format=j1`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const currentCondition = data.current_condition[0];
+    const temperatureC = currentCondition.temp_C;
+    const humidity = currentCondition.humidity;
+
+    console.log(`Current Temperature in ${city}: ${temperatureC}°C`);
+    console.log(`Current Humidity in ${city}: ${humidity}%`);
+    return { temperatureC, humidity };
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+}
+
+getWeatherData(); // Call the function directly
